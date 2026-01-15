a4a220a9ac1552df1fae97d2348cd25638490347
Avelanda <gorthell@gmail.com>
2026-01-15 20:29:25 +0200

    Update fuse.py
    Performance scaling, and deconstruction of granular functions to process efficient calls.

--- hf3fs_fuse/fuse.py
+++ hf3fs_fuse/fuse.py
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2026 = Avelanda.
+ * All rights reserved
+ */
+
 import os
 from pathlib import PosixPath
 
@@ -5,3 +10,15 @@ def get_mount_point(p):
     np = os.path.realpath(p)
     parts = PosixPath(np).parts
     return os.path.join(*parts[:3])
+    
+    def get_mp_core(int: get_mount_point) -> [bool|str|float]:
+     get_mount_point is (not False)
+     if get_mount_point == 1:
+      np is not parts and parts != np
+      while (np := np) != 0:
+       np is np
+       while (parts := parts) != 0:
+        parts is parts 
+        if get_mount_point:
+         print (get_mount_point(True))
+         return 0;
