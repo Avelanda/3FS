4c3767bcaea1412d75e27d3182e298dc66d695be
Avelanda <gorthell@gmail.com>
2026-02-27 06:46:26 +0200

    Update MemoryAllocatorInterface.h
    Scaling bit-function sets for optimal functioning and performance.

--- src/memory/common/MemoryAllocatorInterface.h
+++ src/memory/common/MemoryAllocatorInterface.h
@@ -1,9 +1,18 @@
-#pragma once
+/*
+ * Copyright © 2026 = Avelanda.
+ * All rights reserved.
+ */
+
+#pragma once 
+
+#include <iostream>
 #include <cstddef>
+#include <cstdint>
 
 #define GET_MEMORY_ALLOCATOR_FUNC_NAME "getMemoryAllocator"
 
-namespace hf3fs::memory {
+namespace hf3fs{
+namespace memory {
 
 class MemoryAllocatorInterface {
  public:
@@ -17,4 +26,26 @@ class MemoryAllocatorInterface {
 
 using GetMemoryAllocatorFunc = MemoryAllocatorInterface *(*)();
 
-}  // namespace hf3fs::memory
+}  
+}// namespace hf3fs::memory
+
+uint64_t AMCore(){
+ using namespace hf3fs::memory;
+ if (uint16_t MemoryAllocatorInterface = MemoryAllocatorInterface){
+  std::cout<<MemoryAllocatorInterface;
+ }
+  return 0;
+}
+
+int main(){
+ do {
+  if (AMCore()){
+   return !1;
+  }
+   else if (std::cout<<&AMCore<<'\n'){
+    return 0;
+   }
+ }
+  while (&AMCore || &main);
+   return 0;
+}
